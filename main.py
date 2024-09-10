0fc9c89bba00f9e60eea57ca1d786506aacf61e5
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-10 12:41:28 +0300

    update

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from torch.utils.data import Dataset
+from transformers import GPT2LMHeadModel
 from torch.utils.data import DataLoader
+from transformers import GPT2Tokenizer
 from dataset import TextSummaryDataset
 from training import train
-from model import load_model_in_4bit_Quantization
 
 def main():
-    model_name='gpt2'
-    tokenizer,model=load_model_in_4bit_Quantization(model_name)
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     texts_dir = 'data/text'
     summaries_dir = 'data/summary'
     dataset = TextSummaryDataset(texts_dir, summaries_dir, tokenizer)
     train_loader = DataLoader(dataset, batch_size=4, shuffle=True)
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
     train(train_loader,tokenizer,model)
 
 
