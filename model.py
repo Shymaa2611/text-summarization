e0bca42ef37811ebf2fa5ae749ec99748e71904f
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-10 11:40:38 +0300

    update main

--- model.py
+++ model.py
@@ -1,15 +1,20 @@
-from transformers import AutoModelForCausalLM,AutoTokenizer,BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import torch
+
 def Quantization_Config():
-    bnb_config=BitsAndBytesConfig(
+    bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
-         bnb_4bit_quant_type="nf4",
-         bnb_4bit_compute_dtype=torch.bfloat16
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
     )
     return bnb_config
 
 def load_model_in_4bit_Quantization(model_name):
-    bnb_config=Quantization_Config()
-    tokenizer=AutoTokenizer.from_pretrained(model_name)
-    model=AutoModelForCausalLM.from_pretrained(model=model_name,quantization_config=bnb_config)
-    return tokenizer,model
+    bnb_config = Quantization_Config()
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, 
+        quantization_config=bnb_config,
+        load_in_4bit=True  
+    )
+    return tokenizer, model
